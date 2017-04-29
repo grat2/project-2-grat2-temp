05a7bcc3ef98f6c9f903fb8963365bba6b545b7e
grat2 <gf310031414@gmail.com>
2017-04-28 20:46:23 -0700

    added some code

--- music_catalog.py
+++ music_catalog.py
@@ -1,1 +1,33 @@
 import linked_list.py, array_list.py
+
+# a SongCatalog is a data type of SongCatalog(title, artist, album)
+class SongCatalog:
+    def __init__(self, songList, sortType):
+        self.songList = songList # a List
+        self.sortType = sortType # an integer
+        self.sortedSongs = songList # a List (not yet sorted at time of object construction)
+    def __eq__(self, other):
+        return (type(other) == Song
+            and self.songList == other.songList
+            and self.sortType == other.sortType
+            and self.sortedSongs == other.sortedSongs)
+    def __repr__(self):
+        return "Song({!r}, {!r}, {!r})".format(self.title, self.artist, self.album)
+
+# None -> None
+# displays a menu to carry out various actions involving a song catalog
+def song_catalog():
+    print("Song Catalog \
+        \n   1) Print Catalog \
+        \n   2) Song Information \
+        \n   3) Sort \
+        \n   4) Add Songs \
+        \n   0) Quit \
+        Enter selection: ")
+    userIn = input()
+    if(userIn == 1):
+        print_songs()
+
+# List value -> str
+# returns a string of songs from the given list that is sorted by the given input value
+def print_songs(l, sortType):
